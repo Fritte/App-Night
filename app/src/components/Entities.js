03ce9b9560e28d5fc4a9a460216a19ce91a7c6a2
whoiscris <ga57far@mytum.de>
2015-11-28 23:01:31 +0000

    last man standing

--- app/src/components/Entities.js
+++ app/src/components/Entities.js
@@ -9,10 +9,21 @@ export default class Entities extends Component {
   }
 
   render() {
-    const entities = this.props.entities;
+    var entities = [];
+    if (this.props.entities && this.props.entities.entities) entities = this.props.entities.entities;
+
     return (
       <div>
-        Entities in here!
+        { entities.map( v => <button className="btn">v.text</button>)}
+        {/*
+          !entities.length ? 'nothing' : 
+          entities.reduce( (prev, curr, currIndex) => currIndex % 2 == 0 ? prev.push([curr]) : prev[prev.length-1].push(curr) )
+          .map( v => (
+              <tr>
+                {v.map(vv => (<td>vv.text</td>))}
+              </tr>
+            ))
+        */}
       </div>);
   }
 }
