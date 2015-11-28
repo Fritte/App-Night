a7dc2d31e570feb71130d5cf93e0b833427ef407
Arved Baus <arved.baus@gmail.com>
2015-11-28 20:32:31 +0100

    Serve JSON Objects of twitter posts

--- twitter_grep/index.js
+++ twitter_grep/index.js
@@ -7,31 +7,49 @@ var client = new Twitter({
   access_token_secret: 'lO0jD9iJyHDDsQi0phXwxI7ryirlFgEgZl7Ysxks1EQ1j'
 });
 
+
+
 var params = { 
-  screen_name: 'realdonaldtrump', //'HillaryClinton',
+  screen_name: 'RealBenCarson', // 'HillaryClinton', 'realDonaldTrump', 'RealBenCarson'
   count: 200,
   exclude_replies: true,
   include_rts: false
 };
 
 var callsLeft = 10;
+// produce blob
+var isBlob = process.argv[2] === 'blob'
+var res = []
 
-function loadTweets(start_id) {
+// console.log(isBlob ? 'Concatening tweets to text blob' : 'Concatenating tweets objects'); 
 
+
+function loadTweets(input, start_id) {
   var p = params;
   if (start_id) {
     p.max_id = start_id;
   }
-
   client.get('statuses/user_timeline', p, (error, tweets, response) => {
-      if (!error) {
-        console.log(tweets.map( (t) => t.text ).reduce( (l, t) => l + t + '\n' ));
-        if (callsLeft > 0) {
-          callsLeft--;
-          return loadTweets(tweets[tweets.length-1].id);
-        }
+    if(error){
+      throw Error(" An error occured calling Twitter: " + error); 
+    }
+
+    if(callsLeft == 0) {
+      if(isBlob){
+        console.log(input.map( (t) => t.text ).reduce( (l, t) => l + t + '\n' ));
       }
+      else{
+        var filteredList = input.map( (t) => { return {'text' :  t.text, 'id' : t.id, 
+            'retweet_count' : t.retweet_count, 'source' : t.source, 
+            'favorite_count' : t.favorite_count }; } );
+        console.log(JSON.stringify(filteredList));
+      }
+      return;
+    }
+
+    callsLeft--;
+    loadTweets( input.concat(tweets), tweets[tweets.length-1].id );
   });
 }
 
-loadTweets();
+loadTweets([]);
